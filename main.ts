a6c31fdb76a9fd0c9e70c87148ee4ced6bf25f04
Sudhish Meenakshisundaram <65432331+sudhish3120@users.noreply.github.com>
2020-09-18 16:11:36 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-scene.setBackgroundColor(0)
+let Cars: Sprite = null
+scene.setBackgroundColor(7)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 4 4 4 . . . . . . . 
+    . . . . . 1 4 4 4 1 . . . . . . 
+    . . . . . 1 1 1 1 1 . . . . . . 
+    . f f f f 1 1 1 1 1 f f f f . . 
+    . . . f f 1 1 1 1 1 f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(80, 110)
+game.onUpdateInterval(500, function () {
+    Cars = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+        . f f 8 8 8 8 8 8 8 8 8 f f . . 
+        . f f 8 8 8 8 8 8 8 8 8 f f . . 
+        . f f 8 8 8 8 8 8 8 8 8 f f . . 
+        . f f 8 8 8 8 8 8 8 8 8 f f . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
+        . f f f 8 8 8 8 8 8 8 8 f f . . 
+        . f f f 8 8 8 8 8 8 8 8 f f . . 
+        . f f f 8 8 8 8 8 8 8 8 f f . . 
+        . f f f 8 8 8 8 8 8 8 8 f f . . 
+        . . 8 8 8 8 8 8 8 8 8 8 f f . . 
+        . 8 8 8 8 8 8 8 8 8 8 8 8 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
