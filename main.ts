b9bf2a10e18b48a20e2508b61e3075f6676f8ac2
shouza1712 <71526713+shouza1712@users.noreply.github.com>
2020-09-25 15:49:16 +0530

    Just changed the design of our car to look more realistic

--- main.ts
+++ main.ts
@@ -22,24 +22,24 @@ let goose = sprites.create(img`
 goose.setPosition(80, 110)
 game.onUpdateInterval(500, function () {
     Cars = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
-        . f f 8 8 8 8 8 8 8 8 8 f f . . 
-        . f f 8 8 8 8 8 8 8 8 8 f f . . 
-        . f f 8 8 8 8 8 8 8 8 8 f f . . 
-        . f f 8 8 8 8 8 8 8 8 8 f f . . 
-        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
-        . . 8 8 8 8 8 8 8 8 8 8 8 . . . 
-        . f f f 8 8 8 8 8 8 8 8 f f . . 
-        . f f f 8 8 8 8 8 8 8 8 f f . . 
-        . f f f 8 8 8 8 8 8 8 8 f f . . 
-        . f f f 8 8 8 8 8 8 8 8 f f . . 
-        . . 8 8 8 8 8 8 8 8 8 8 f f . . 
-        . 8 8 8 8 8 8 8 8 8 8 8 8 . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        . . . . . . e e c c e e . . . . 
+        . . . . . e 2 2 2 2 2 2 e . . . 
+        . . . . 2 c 2 2 2 2 2 2 c 2 . . 
+        . . . e 2 c 4 2 2 2 2 2 c 2 e . 
+        . . . f 2 2 4 2 2 2 2 2 c 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 2 4 2 2 2 2 2 2 2 f . 
+        . . . f 2 c 2 4 4 2 2 2 c 2 f . 
+        . . . e 2 c e c c c c e c 2 e . 
+        . . . e 2 e c b b b b c e 2 e . 
+        . . . e 2 e b b b b b b e 2 e . 
+        . . . e e e e e e e e e e e e . 
+        . . . f e d e e e e e e d e f . 
+        . . . f e 2 d e e e e d 2 e f . 
+        . . . f f e e e e e e e e f f . 
+        . . . . f f . . . . . . f f . . 
         `, SpriteKind.Player)
-    Cars.setPosition(70, 0)
+    Cars.setPosition(66, 13)
     red_test = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . 2 2 2 . . . . . . 
